a5643b56cdd5cc0968e5ff53af1434d286e46a8e
Aida <shakharova.aida@yandex.ru>
2022-10-30 12:30:45 +0600

    map

---
+++ 2.py
@@ -0,0 +1,19 @@
+# В файле находится N натуральных чисел, записанных через пробел. 
+# Среди чисел не хватает одного, чтобы выполнялось условие A[i] - 1 = A[i-1]. Найдите это число.
+# 1 2 3 5
+
+num = ''
+
+def fun(num):
+    for i in range(0,len(num)):
+        if (num[i+1])-num[i] != 1:
+            return num[i]+1
+
+
+with open('output.txt', 'r') as data:
+    
+    num = data.read()
+
+num = list(map(int, num.split()))
+
+print(fun(num))
